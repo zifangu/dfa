2e4ff9233658f3ac2e29c876f8c7234d1a2a95d7
Ivan Gu <guz@email.wofford.edu>
2020-02-19 22:53:21 -0500

    Initial commit

---
+++ dfa.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""
+1. read in dfa.txt
+2. put states, alphabets, start states, final states into separate lists
+3. rules: create a dictionary of a 2-tuple (start, path) and end from dfa.txt
+3.5 read in input.txt. start collecting paths
+4. create a function that take the 2-tuple, and return end state
+5. the returned state is in a new 2-tuple (returned state, next char in file)
+6. loop until the last char on the line is reached
+7. compare the returned result to the list of final states
+
+"""
+
+
+
+def open_file(filename):
+    file = open(filename, "r")
+    content = file.read()
+    return content
+
+
+def main():
+    dfa = open_file("dfa.txt")
+    # states = []
+    # alphabets = []
+    # start_states = []
+    # final_states = []
+    # transition_rules = []
+
+
+
+
+
+
+
+
+
+
+main()
+
